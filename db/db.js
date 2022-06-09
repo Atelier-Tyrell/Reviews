213fe1c5a247f1f2c55184f8dcbdee49bdb8abd7
Jordan Sweet <hello@jordandsweet.com>
2022-06-09 00:34:19 -0700

    Use db.js to load the pg Pool

---
+++ db/db.js
@@ -0,0 +1,20 @@
+/*
+ * Creates the pool connection to Postgres.
+*/
+
+require('dotenv').config();
+
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  'user': process.env.POSTGRES_USER,
+  'password': process.env.POSTGRES_PASSWORD,
+  'database': process.env.DATABASE
+})
+
+pool.on('error', (error, client) => {
+  console.error('Unexpected error on idle client', err);
+  process.exit(-1);
+});
+
+module.exports.pool = pool;
