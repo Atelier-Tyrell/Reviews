e4f1627834cf2df53f8f45f012a9c8c9a19f22fd
Jordan Sweet <hello@jordandsweet.com>
2022-06-12 12:25:50 -0700

    Clean up formatting

--- server-js/routes.js
+++ server-js/routes.js
@@ -1,19 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const controllers = require('./controllers/controllers');
+const ctrls = require('./controllers/controllers');
 
-router.get('/reviews/', (req, res) => controllers.reviews(req, res));
-
-router.get('/reviews/meta', (req, res) => controllers.metadata(req, res));
-
-router.post('/reviews', (req, res) => controllers.add(req, res));
-
-router.put('/reviews/:review_id/helpful', (req, res) =>
-  controllers.markHelpful(req, res)
-);
-
-router.put('/reviews/:review_id/report', (req, res) =>
-  controllers.reportReview(req, res)
-);
+router.get('/reviews/', (req, res) => ctrls.reviews(req, res));
+router.get('/reviews/meta', (req, res) => ctrls.metadata(req, res));
+router.post('/reviews', (req, res) => ctrls.add(req, res));
+router.put('/reviews/:review_id/helpful', (req, res) => ctrls.markHelpful(req, res));
+router.put('/reviews/:review_id/report', (req, res) => ctrls.reportReview(req, res));
 
 module.exports = router;
