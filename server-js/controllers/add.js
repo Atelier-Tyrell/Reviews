3eac915bdc2b35ff45b2be790648827fac6a9ed7
Jordan Sweet <hello@jordandsweet.com>
2022-06-12 12:03:24 -0700

    Refactor controllers in to separate files

---
+++ server-js/controllers/add.js
@@ -0,0 +1,8 @@
+const models = require('./models');
+
+const add = async (req, res) => {
+  console.log(req.body);
+  res.sendStatus(201);
+}
+
+module.exports = add;
---
+++ server-js/controllers/metadata.js
@@ -0,0 +1,14 @@
+const models = require('./models');
+
+const metadata = async (req, res) => {
+  const id = req.query.id;
+  try {
+    const query = await models.metadata(id);
+    res.status(200).send(query[0].json_build_object);
+  } catch (error) {
+    console.error(error);
+    res.status(504).send(error);
+  }
+}
+
+module.exports = metadata;
