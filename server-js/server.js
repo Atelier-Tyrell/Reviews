635fb841e92b5ba3024c3dc6ac36a7c9f391fdf7
Jordan Sweet <hello@jordandsweet.com>
2022-06-12 02:02:44 -0700

    Point routes to controllers

--- server-js/server.js
+++ server-js/server.js
@@ -11,25 +11,20 @@ const app = express();
 app.use(express.json());
 app.use(morgan('combined'));
 
+
 /* Routes */
 app.get('/reviews/', (req, res) => controllers.getReviews(req, res));
 
 app.get('/reviews/meta', (req, res) => controllers.getMetadata(req, res));
 
-app.post('/reviews', (req, res) => {
-  // Body Parameters
-});
+app.post('/reviews', (req, res) => controllers.addReview(req, res));
 
-app.put('/reviews/:review_id/helpful', (req, res) => {
-  // Parameters
-  // review_id: integer the review to update
-});
+app.put('/reviews/:review_id/helpful', (req, res) => controllers.markReviewHelpful(id));
 
-app.put('/reviews/:review_id/report', (req, res) => {
-  // Parameters
-  // review_id: integer the review to update
-});
+app.put('/reviews/:review_id/report', (req, res) => controllers.reportReview(id));
 
+
+/* Launch */
 app.listen(process.env.SERVER_PORT, () => {
   console.log(`Listening on port ${process.env.SERVER_PORT}...`);
 });
