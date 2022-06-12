72a18aa8588d8c20db7b94a258b58e6ec28037da
Jordan Sweet <hello@jordandsweet.com>
2022-06-11 20:38:34 -0700

    Add metadata models

--- server-js/models.js
+++ server-js/models.js
@@ -1,21 +1,68 @@
 const pool = require('../db/db');
 const format = require('pg-format');
 
-console.log(pool);
-
 const getReviews = async (id, page, count, sort) => {
   const offset = (page - 1) * count;
-  const query = await pool.query(
-    `SELECT * FROM reviews.reviews
-     WHERE product_id = $1
-     ORDER BY $2
-     LIMIT $3
-     OFFSET $4`,
-    [id, sort, count, offset]
-  );
-  return query;
+  try {
+    const query = await pool.query(
+      `SELECT * FROM reviews.reviews, reviews.photos
+       WHERE product_id = $1
+       ORDER BY $2
+       LIMIT $3
+       OFFSET $4`,
+      [id, sort, count, offset]
+    );
+    return query;
+  } catch (error) {
+    return error;
+  }
 };
+
+
+const getMetadata = async (id) => {
+  try {
+    const query = await pool.query(
+      `SELECT (
+         id,
+         num_1_stars,
+         num_2_stars,
+         num_3_stars,
+         num_4_stars,
+         num_5_stars,
+         fit_id,
+         width_id,
+         length_id,
+         comfort_id,
+         quality_id,
+         quality_total::float / num_reviews,
+         fit_total::float / num_reviews,
+         length_total::float / num_reviews,
+         comfort_total::float / num_reviews,
+         quality_total::float / num_reviews,
+         num_recommended
+       )
+       FROM reviews.products
+       WHERE id = $1`,
+      [id]
+    );
+    console.log(query.rows);
+  } catch (error) {
+    throw error;
+    console.log(error);
+  }
+}
+
+const addReview = async () => {
+}
+
+const markReviewHelpful = async () => {
+}
+
+const reportReview = async () => {
+}
+
 
 module.exports = {
   getReviews,
+  getMetadata
 }
