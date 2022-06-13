afa375bea6902aa62b8a26236f1a332d8818ae0a
Jordan Sweet <hello@jordandsweet.com>
2022-06-13 13:47:20 -0700

    Format route url

--- server-js/routes.js
+++ server-js/routes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const ctrls = require('./controllers/controllers');
 
-router.get('/reviews/', (req, res) => ctrls.reviews(req, res));
+router.get('/reviews', (req, res) => ctrls.reviews(req, res));
 router.get('/reviews/meta', (req, res) => ctrls.metadata(req, res));
 router.post('/reviews', (req, res) => ctrls.add(req, res));
 router.put('/reviews/:review_id/helpful', (req, res) => ctrls.markHelpful(req, res));
